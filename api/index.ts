48ebf70a739a24217f7c1390ba299cfba9cfd283
felix-gustavo <gustavovieira1945@gmail.com>
2024-09-19 09:07:00 -0300

    create api folder

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
-import { app } from './config/app.ts'
+import { app } from '../src/config/app.ts'
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+export default app
