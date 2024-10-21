1b15e77e769f4f56140180ea3b8495863c0fc1f1
felix-gustavo <gustavovieira1945@gmail.com>
2024-10-21 10:50:38 -0300

    add toZonedTime in MedicalScheduler

--- src/services/amazonSchedulers/medicalScheduler.ts
+++ src/services/amazonSchedulers/medicalScheduler.ts
@@ -2,7 +2,7 @@ import { AmazonScheduler } from './scheduler'
 import { CreateScheduleCommandInput } from '@aws-sdk/client-scheduler'
 import { MedicalReminder } from '../medicalReminderService/iMedicalReminderService'
 import { format } from 'date-fns'
-import { fromZonedTime } from 'date-fns-tz'
+import { fromZonedTime, toZonedTime } from 'date-fns-tz'
 
 type MedicalSchedulerInput = {
   ask_user_id: string
@@ -18,7 +18,7 @@ export class MedicalScheduler extends AmazonScheduler<MedicalSchedulerInput> {
     return {
       Name: this.scheduleName(data.input.id), // required
       ScheduleExpression: `at(${format(
-        data.input.datetime,
+        toZonedTime(data.input.datetime, timezone),
         "yyyy-MM-dd'T'HH:mm:ss",
       )})`, // required
       ScheduleExpressionTimezone: timezone,
